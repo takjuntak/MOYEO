accec66fe4f184a2b85b103f1f704a2980b30766
정지원 <littlesam95@naver.com>
2025-01-22 10:19:40 +0900

    Commit

--- Android/TravelTogether/app/src/main/java/com/neungi/moyeo/views/albumdetail/AlbumViewModel.kt
+++ Android/TravelTogether/app/src/main/java/com/neungi/moyeo/views/albumdetail/AlbumViewModel.kt
@@ -1,13 +1,11 @@
-package com.neungi.moyeo.views
+package com.neungi.moyeo.views.albumdetail
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
+class AlbumViewModel @Inject constructor(
 
 ) : ViewModel() {
-
-
 }
