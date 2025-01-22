41cdb583badd6b411e81a815ee47575d79b58d18
최미연 <mu05041@naver.com>
2025-01-22 12:53:37 +0900

    feat : Add /TravelTogether/trip/

---
+++ BackEnd/TravelTogether/src/main/java/com/travel/together/TravelTogether/global/TripMember.java
@@ -0,0 +1,29 @@
+package com.travel.together.TravelTogether.global;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "trip_member")
+@Getter
+@Setter
+@NoArgsConstructor
+public class TripMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id")
+    private Trip trip;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "is_owner")
+    private Boolean isOwner;
+}
+
