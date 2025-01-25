df6112a2b0e4d81d0efdc33fa9b29d65b318fdf3
정지원 <littlesam95@naver.com>
2025-01-26 01:28:17 +0900

    [feat] Join 화면 UI 구현 S12P11D210-70

--- Android/TravelTogether/app/src/main/java/com/neungi/moyeo/util/CommonUtils.kt
+++ Android/TravelTogether/app/src/main/java/com/neungi/moyeo/util/CommonUtils.kt
@@ -3,9 +3,12 @@ package com.neungi.moyeo.util
 object CommonUtils {
 
     private val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}\$".toRegex()
-    private val passwordRegex = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z\\\\d!@#$%^&*(),.?\":{}|<>]{8,16}$".toRegex()
+    private val passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_=+{}\\[\\]|;:'\",.<>?/]).{8,16}$".toRegex()
+    private val phoneNumberRegex = "^010-\\d{4}-\\d{4}\$".toRegex()
 
     fun validateEmail(email: CharSequence): Boolean = emailRegex.matches(email)
 
     fun validatePassword(password: CharSequence): Boolean = passwordRegex.matches(password)
+
+    fun validatePhoneNumber(phoneNumber: CharSequence): Boolean = phoneNumberRegex.matches(phoneNumber)
 }
