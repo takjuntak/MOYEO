6920cb8f54e48e497050a876e5d65cc5bd30c4d4
정지원 <littlesam95@naver.com>
2025-01-27 14:00:24 +0900

    [feat] 사진별 추억 수정 및 삭제 UI 추가 S12P11D210-60
     - 수정 버튼을 누르면 추억 편집 UI로 변경
     - 삭제 버튼을 누르면 삭제를 요청하는 Dialog 출력, 삭제 가능
      - 서버 통신 부분은 일단 명세서대로 작성했으나, Comment의 Id, 그리고 수정 시에는 Comment의 Content만 전달하면 될 것으로 예상됨

--- Android/TravelTogether/app/src/main/java/com/neungi/moyeo/views/album/adapter/PhotoCommentAdapter.kt
+++ Android/TravelTogether/app/src/main/java/com/neungi/moyeo/views/album/adapter/PhotoCommentAdapter.kt
@@ -1,6 +1,7 @@
 package com.neungi.moyeo.views.album.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -18,6 +19,49 @@ class PhotoCommentAdapter(
         fun bind(comment: Comment, viewModel: AlbumViewModel) {
             binding.comment = comment
             binding.vm = viewModel
+
+            // 수정
+            binding.ivModifyPhotoComment.setOnClickListener {
+                with(binding) {
+                    tvContentPhotoComment.visibility = View.GONE
+                    ivModifyPhotoComment.visibility = View.GONE
+                    ivDeletePhotoComment.visibility = View.GONE
+                    etPhotoComment.visibility = View.VISIBLE
+                    ivUpdatePhotoComment.visibility = View.VISIBLE
+                    ivCancelPhotoComment.visibility = View.VISIBLE
+                    etPhotoComment.setText(comment.content)
+                }
+            }
+
+            // 수정 취소
+            binding.ivCancelPhotoComment.setOnClickListener {
+                with(binding) {
+                    tvContentPhotoComment.visibility = View.VISIBLE
+                    ivModifyPhotoComment.visibility = View.VISIBLE
+                    ivDeletePhotoComment.visibility = View.VISIBLE
+                    etPhotoComment.visibility = View.GONE
+                    ivUpdatePhotoComment.visibility = View.GONE
+                    ivCancelPhotoComment.visibility = View.GONE
+                }
+            }
+
+            // 수정 완료
+            binding.ivUpdatePhotoComment.setOnClickListener {
+                with(binding) {
+                    tvContentPhotoComment.visibility = View.VISIBLE
+                    ivModifyPhotoComment.visibility = View.VISIBLE
+                    ivDeletePhotoComment.visibility = View.VISIBLE
+                    etPhotoComment.visibility = View.GONE
+                    ivUpdatePhotoComment.visibility = View.GONE
+                    ivCancelPhotoComment.visibility = View.GONE
+                }
+                viewModel.onClickCommentUpdate(comment)
+            }
+
+            // 삭제
+            binding.ivDeletePhotoComment.setOnClickListener {
+                viewModel.onClickCommentDelete(comment)
+            }
         }
     }
 
