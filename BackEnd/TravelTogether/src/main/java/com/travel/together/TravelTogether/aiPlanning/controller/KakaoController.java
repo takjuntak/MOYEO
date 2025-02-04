8d5bd820419f55f37bfe6b936ad7bcabaac006c7
juntak <jjt6759@gmail.com>
2025-02-04 10:48:38 +0900

    feat: KakaoController ãPOS -> GET 코 수정

--- BackEnd/TravelTogether/src/main/java/com/travel/together/TravelTogether/aiPlanning/controller/KakaoController.java
+++ BackEnd/TravelTogether/src/main/java/com/travel/together/TravelTogether/aiPlanning/controller/KakaoController.java
@@ -2,12 +2,14 @@ package com.travel.together.TravelTogether.aiPlanning.controller;
 
 import com.travel.together.TravelTogether.aiPlanning.dto.KakaoRequestDto;
 import com.travel.together.TravelTogether.aiPlanning.dto.KakaoResponseDto;
+import com.travel.together.TravelTogether.aiPlanning.dto.KeywordSearchRequestDto;
+import com.travel.together.TravelTogether.aiPlanning.dto.KeywordSearchResponseDto;
 import com.travel.together.TravelTogether.aiPlanning.service.KakaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/kakao")
@@ -20,14 +22,16 @@ public class KakaoController {
     }
 
     // 키워드로 장소 검색하는 엔드포인트
-    @PostMapping("/search")
-    public KakaoResponseDto searchPlace(@RequestBody KakaoRequestDto requestDto) {
+    @GetMapping("/search")
+    public ResponseEntity<KakaoResponseDto> searchImageUrl(@RequestParam String keyword) {
         try {
-            return kakaoService.searchByKeyword(requestDto);
-        } catch (Exception e) {
+            KakaoRequestDto requestDto = new KakaoRequestDto(keyword);
+            KakaoResponseDto responseDto = kakaoService.searchByKeyword(requestDto);
+            return ResponseEntity.ok(responseDto);
+        } catch (IOException e) {
             e.printStackTrace();
-            // 에러 처리 (적절한 에러 메시지를 반환할 수 있습니다)
-            return new KakaoResponseDto("Error",0.0,0.0);
+            return ResponseEntity.internalServerError().body(new KakaoResponseDto("Error fetching data", 0.0, 0.0));
         }
     }
+
 }
