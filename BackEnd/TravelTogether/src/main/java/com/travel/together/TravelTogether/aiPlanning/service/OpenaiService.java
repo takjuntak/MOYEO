d8441ceb36fab18f46ea47077de5d3364d550370
juntak <jjt6759@gmail.com>
2025-02-06 10:03:05 +0900

    refactor: prompt message delete

--- BackEnd/TravelTogether/src/main/java/com/travel/together/TravelTogether/aiPlanning/service/OpenaiService.java
+++ BackEnd/TravelTogether/src/main/java/com/travel/together/TravelTogether/aiPlanning/service/OpenaiService.java
@@ -43,36 +43,7 @@ public class OpenaiService {
                     + "places:" + requestDTO.getPreferences().getPlaces()
                     + "theme:" + requestDTO.getPreferences().getTheme()
                     // 프롬프트 조건
-                    + "일정 생성 규칙\n" +
-                    "1. 여행 순서\n" +
-                    "- destination 배열의 인덱스 순서대로 관광 진행\n" +
-                    "\n" +
-                    "2. 식사 일정\n" +
-                    "- 점심 식사, 저녁 식사만 포함\n" +
-                    "- 각 식사는 90분 소요\n" +
-                    "- 식사의 name은 \"식사\"로 통일\n" +
-                    "\n" +
-                    "3. 관광지 추천 우선순위\n" +
-                    "- 1순위: preferences.activities ('축제'가 존재할 경우 축제를 1순위로 적용)\n" +
-                    "- 2순위: preferences.interests (배열 인덱스 순서대로 우선순위 적용)\n" +
-                    "- name은 구체적인 장소명으로 작성 (예: \"북한산국립공원\", \"국립중앙박물관\")\n" +
-                    "\n" +
-                    "4. 도시 간 이동\n" +
-                    "- 다른 도시로 이동 시 \"이동\" 일정 추가\n" +
-                    "- 방문했던 도시는 다시 방문하지 않는다.\n" +
-                    "\n" +
-                    "5. 응답 형식\n" +
-                    "- JSON 형식으로만 응답\n" +
-                    "- 부가 설명 없이 JSON 데이터만 제공\n" +
-                    "\n" +
-                    "6. 관광 일정\n" +
-                    "- 점심 식사 전의 duration 합은 200 이하로 설정\n" +
-                    "- 점심 식사 이후 저녁 식사 전까지 duration의 합은 240\n" +
-                    "- 저녁 식사 이후 일정의 duration의 합은 120 이하\n" +
-                    "\n" +
-                    "7. 축제 일정\n" +
-                    "- 일정에 축제가 포함된다면 축제의 duration은 120으로 고정.\n" +
-                    "\n" ;
+                    ;
 
             JSONArray messages = new JSONArray();
             JSONObject message = new JSONObject();
