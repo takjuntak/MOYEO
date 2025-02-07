d2c9d1e4be4eb14fafe45e29fd9c26d5defdbddb
juntak <jjt6759@gmail.com>
2025-02-07 11:15:46 +0900

    feat: DirectionsAPI data structure modify

--- BackEnd/TravelTogether/src/main/java/com/travel/together/TravelTogether/aiPlanning/service/DirectionsService.java
+++ BackEnd/TravelTogether/src/main/java/com/travel/together/TravelTogether/aiPlanning/service/DirectionsService.java
@@ -8,11 +8,14 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DirectionsService {
@@ -30,13 +33,40 @@ public class DirectionsService {
                 "&goal=" + directionsRequestDto.getEndLongitude() + "," + directionsRequestDto.getEndLatitude();
 
         JSONObject responseJson = sendRequest(url);
+
+        // 좌표 배열 생성
+        JSONArray pathArray = responseJson.getJSONObject("route")
+                .getJSONArray("traoptimal")
+                .getJSONObject(0)
+                .getJSONArray("path");
+
+        List<DirectionsResponseDto.PathPoint> path = new ArrayList<>();
+
+        for (int i = 0; i < pathArray.length(); i++) {
+            JSONArray pointArray = pathArray.getJSONArray(i);
+            Double longitude = pointArray.getDouble(0);
+            Double latitude = pointArray.getDouble(1);
+            path.add(new DirectionsResponseDto.PathPoint(latitude, longitude));
+        }
+
+        // 총 소요시간
         Integer totalTime = responseJson.getJSONObject("route")
                 .getJSONArray("traoptimal")
                 .getJSONObject(0)  // 첫 번째 항목 가져오기
                 .getJSONObject("summary")
                 .getInt("duration")
                 /60000; // 밀리초 -> 분 단위 변환
-        return new DirectionsResponseDto(totalTime);
+
+        // DirectionPath 객체 생성 및 데이터 설정
+        DirectionsResponseDto.DirectionPath directionPath = new DirectionsResponseDto.DirectionPath();
+        directionPath.setPath(path);
+
+        // DTO 객체 생성 및 값 설정
+        DirectionsResponseDto responseDto = new DirectionsResponseDto();
+        responseDto.setTotalTime(totalTime);
+        responseDto.setDirectionPath(directionPath); // 변경된 변수명 사용
+
+        return responseDto;
     }
 
     private JSONObject sendRequest(String apiUrl) {
