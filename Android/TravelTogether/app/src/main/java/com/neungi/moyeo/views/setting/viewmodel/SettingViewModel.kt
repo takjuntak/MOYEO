1ba8adec8e692994049d477db59a4d1766c3bf1c
정지원 <littlesam95@naver.com>
2025-02-08 23:46:39 +0900

    [feat] 로그인 화면 구현
     - 추가로 로그인 이후 환경설정 ViewModel에 회원 정보 저장하도록 수정

--- Android/TravelTogether/app/src/main/java/com/neungi/moyeo/views/setting/viewmodel/SettingViewModel.kt
+++ Android/TravelTogether/app/src/main/java/com/neungi/moyeo/views/setting/viewmodel/SettingViewModel.kt
@@ -1,8 +1,9 @@
 package com.neungi.moyeo.views.setting.viewmodel
 
 import android.app.Application
+import android.net.Uri
+import androidx.core.net.toUri
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.neungi.domain.usecase.GetUserInfoUseCase
 import com.neungi.domain.usecase.SetUserInfoUseCase
@@ -15,6 +16,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -31,8 +33,20 @@ class SettingViewModel @Inject constructor(
     val settingUiEvent = _settingUiEvent.asSharedFlow()
 
     /*** Datas ***/
-    private val _userName = MutableStateFlow<String>("")
+    private val _userId = MutableStateFlow<String?>(null)
+    val userId = _userId.asStateFlow()
+
+    private val _userName = MutableStateFlow<String?>(null)
     val userName = _userName.asStateFlow()
+
+    private val _userEmail = MutableStateFlow<String?>(null)
+    val userEmail = _userEmail.asStateFlow()
+
+    private val _userProfileMessage = MutableStateFlow<String?>(null)
+    val userProfileMessage = _userProfileMessage.asStateFlow()
+
+    private val _userProfileImage = MutableStateFlow<Uri?>(null)
+    val userProfileImage = _userProfileImage.asStateFlow()
 
     override fun onClickLogin() {
         viewModelScope.launch {
@@ -47,14 +61,39 @@ class SettingViewModel @Inject constructor(
         }
     }
 
+    private fun fetchUserId(): Flow<String?> = flow {
+        val id = getUserInfoUseCase.getUserId().first()
+        emit(id)
+    }
+
     private fun fetchUserName(): Flow<String?> = flow {
         val name = getUserInfoUseCase.getUserName().first()
         emit(name)
     }
 
+    private fun fetchUserEmail(): Flow<String?> = flow {
+        val email = getUserInfoUseCase.getUserEmail().first()
+        emit(email)
+    }
+
+    private fun fetchUserProfileMessage(): Flow<String?> = flow {
+        val message = getUserInfoUseCase.getUserProfileMessage().first()
+        emit(message)
+    }
+
+    private fun fetchUserProfileImage(): Flow<String?> = flow {
+        val profileImage = getUserInfoUseCase.getUserProfile().first()
+        emit(profileImage)
+    }
+
     fun getUserInfo() {
         viewModelScope.launch {
-            _userName.value = fetchUserName().first()?:""
+            _userId.value = fetchUserId().first()
+            _userName.value = fetchUserName().first()
+            _userEmail.value = fetchUserEmail().first()
+            _userProfileMessage.value = fetchUserProfileMessage().first()
+            _userProfileImage.value = fetchUserProfileImage().first()?.toUri()
+            Timber.d("Id: ${_userId.value}")
         }
     }
 }
