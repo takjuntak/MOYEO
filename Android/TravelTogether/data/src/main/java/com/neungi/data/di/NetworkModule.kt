d126af88191884d2993b3ddbd0b23a09f7bba272
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2025-02-10 15:50:31 +0900

    [style] 지역 선택 UI 수정

--- Android/TravelTogether/data/src/main/java/com/neungi/data/di/NetworkModule.kt
+++ Android/TravelTogether/data/src/main/java/com/neungi/data/di/NetworkModule.kt
@@ -24,9 +24,9 @@ import javax.inject.Singleton
 object NetworkModule {
 
 //    private const val BASE_URL3 = "http://43.202.51.112:8081/"
-    private const val BASE_URL = "http://43.202.51.112:8081/"
+//    private const val BASE_URL = "http://43.202.51.112:8081/"
 //    private const val WEBSOCKET_URL = "ws://43.202.51.112:8080/"
-//    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     @Provides
     @Singleton
