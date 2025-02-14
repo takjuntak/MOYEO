f4578216d9314dc94bf7c1c1da81dedcb343051f
Daekyu Kang <ethan2024@naver.com>
2025-02-14 14:28:58 +0900

    오류 찾기

--- BackEnd/TravelTogether/src/main/java/com/travel/together/TravelTogether/config/EnvironmentLogger.java
+++ BackEnd/TravelTogether/src/main/java/com/travel/together/TravelTogether/config/EnvironmentLogger.java
@@ -4,8 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
 import java.util.Arrays;
-import java.util.Map;
 
 @Slf4j
 @Component
