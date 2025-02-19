6d5dfae8a3bad00c3665e2a6efdea58cf19cb760
areum <rladkfma9988@gmail.com>
2025-02-19 12:46:16 +0900

    [refactor] recyclerview item ui

--- Android/TravelTogether/app/src/main/java/com/neungi/moyeo/util/Section.kt
+++ Android/TravelTogether/app/src/main/java/com/neungi/moyeo/util/Section.kt
@@ -16,7 +16,6 @@ data class Section(
 fun convertToSections(scheduleReceive: ScheduleReceive, trip: Trip): MutableList<Section> {
 
     val sections = mutableListOf<Section>()
-    Timber.d("${scheduleReceive.day}")
     scheduleReceive.day.forEachIndexed { index, day ->
         val sectionHeader = ScheduleHeader(
             dayId = index + 1,
@@ -30,7 +29,6 @@ fun convertToSections(scheduleReceive: ScheduleReceive, trip: Trip): MutableList
             // 경계값과 동일한 일정이 생겨도 헤더가 위로 오게 하기위함 Item의 path값을 계산할땐 +1해서 계산
         // 경계값과 동일한 일정이 생겨도 헤더가 위로 오게 하기위함 Item의 path값을 계산할땐 +1해서 계산
         )
-        println("startTime ${day.startTime}")
         // 각 Day의 schedule을 items로 설정
         val sectionItems = day.schedules.toMutableList()
         // Section을 생성하여 List에 추가
@@ -38,7 +36,6 @@ fun convertToSections(scheduleReceive: ScheduleReceive, trip: Trip): MutableList
             head = sectionHeader,
             items = sectionItems
         )
-        println("convert "+section)
         sections.add(section)
     }
 
